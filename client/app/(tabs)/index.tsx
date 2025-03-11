f64f25af0d89ec389acb85bdcc78c7bc2ab0f723
Hyaguiin <137925587+Hyaguiin@users.noreply.github.com>
2025-03-11 15:38:34 -0300

    feat: client

---
+++ client/app/(tabs)/index.tsx
@@ -0,0 +1,15 @@
+import { Image, StyleSheet, Platform } from 'react-native';
+
+import { HelloWave } from '@/components/HelloWave';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import { Home } from '@/pages/home/home';
+
+
+export default function HomeScreen() {
+  return (
+    <Home/>
+      
+  );
+}
---
+++ client/pages/home/home.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export const Home = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>SHREK gosta de SEXO</Text>
+      <Text style={styles.description}>This is the home page.</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  description: {
+    fontSize: 16,
+    color: '#555',
+  },
+});
