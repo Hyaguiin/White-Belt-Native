e627a52f1b38ce022c8afa321fe00d844a1b5032
Márcio Vitor <vitor.mert@outlook.com>
2025-06-10 09:11:07 -0300

    Update JWT.ts

--- api/src/utils/JWT.ts
+++ api/src/utils/JWT.ts
@@ -7,7 +7,7 @@ interface JwtPayload {
   role: string;
 }
 
-const JWT_SECRET = process.env.JWT_SECRET;
+const JWT_SECRET = process.env.JWT_SECRET as string;
 
 if (!JWT_SECRET) {
   throw new Error('JWT_SECRET is not defined in .env');
